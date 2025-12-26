8ed610c33a885b5d8a98bf8a8cba840d58223e47
Arthur Heidt <arthur@arthurheidt.nl>
2025-12-26 10:37:08 +0100

    day 4: naive

---
+++ day-4/src/main.rs
@@ -0,0 +1,69 @@
+fn main() {
+    let input = include_str!("./input_example.txt");
+    let grid = parse_input(input);
+    println!(
+        "Total accessible places (naive): {}",
+        total_accessible_places_naive(&grid)
+    );
+}
+
+// The grid is modeled as a 2d vector of booleans,
+// where True indicates a paper roll
+fn parse_input(input: &str) -> Vec<Vec<bool>> {
+    input
+        .lines()
+        .map(|line| line.trim().chars().map(|c| c == '@').collect())
+        .collect()
+}
+
+const NEIGHBOUR_OFFSETS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+fn get_occupied_neighbours(grid: &[Vec<bool>], y: usize, x: usize) -> usize {
+    let mut occupied_neighbours = 0;
+    for (y_off, x_off) in NEIGHBOUR_OFFSETS {
+        let y_n = y as isize + y_off;
+        let x_n = x as isize + x_off;
+
+        if y_n < 0 || x_n < 0 {
+            continue;
+        }
+
+        if y_n as usize >= grid.len() {
+            continue;
+        }
+
+        let row = &grid[y_n as usize];
+
+        if x_n as usize >= row.len() {
+            continue;
+        }
+
+        if row[x_n as usize] {
+            occupied_neighbours += 1;
+        }
+    }
+
+    occupied_neighbours
+}
+
+fn total_accessible_places_naive(grid: &[Vec<bool>]) -> usize {
+    let mut accessible_count = 0;
+    for (y, line) in grid.iter().enumerate() {
+        for (x, spot) in line.iter().enumerate() {
+            if *spot && get_occupied_neighbours(grid, y, x) < 4 {
+                accessible_count += 1;
+            }
+        }
+    }
+
+    accessible_count
+}
