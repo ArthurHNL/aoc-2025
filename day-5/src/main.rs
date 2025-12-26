9b3e39f0f6d905e6c8e773baa5913d19a0dbe271
Arthur Heidt <arthur@arthurheidt.nl>
2025-12-26 12:47:10 +0100

    day 5

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,16 +1,19 @@
+use std::cmp::max;
 use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("./input.txt");
-    let (fresh_ids, requested_ids) = parse_input(input);
+    let (fresh_id_ranges, requested_ids) = parse_input(input);
 
-    println!("Fresh ids: {:?}", fresh_ids);
+    println!("Fresh ids: {:?}", fresh_id_ranges);
     println!("Requested ids: {:?}", requested_ids);
 
     println!(
         "Count of requested ids that are fresh (naive): {}",
-        requested_fresh_ids_count_naive(&fresh_ids, &requested_ids)
+        requested_fresh_ids_count_naive(&fresh_id_ranges, &requested_ids)
     );
+
+    println!("Count of fresh ids: {}", fresh_id_count(&fresh_id_ranges));
 }
 
 fn parse_fresh_id_set_line(input_line: &str) -> (usize, usize) {
@@ -25,9 +28,9 @@ fn parse_fresh_id_set_line(input_line: &str) -> (usize, usize) {
         .unwrap()
 }
 
-// The input is modeled as a pair of hash sets, where the first set contains the fresh ingredient
-// ids and the second set contains the requested ids.
-fn parse_input(input: &str) -> (HashSet<(usize, usize)>, HashSet<usize>) {
+// The input is modeled as a pair with the first being a vector of input id ranges
+// and the second a hash set of requested ids.
+fn parse_input(input: &str) -> (Vec<(usize, usize)>, HashSet<usize>) {
     let trimmed_input: Vec<&str> = input.lines().map(|l| l.trim()).collect();
     let split_ix = trimmed_input
         .iter()
@@ -35,14 +38,12 @@ fn parse_input(input: &str) -> (HashSet<(usize, usize)>, HashSet<usize>) {
         .find(|(_, line)| line.is_empty())
         .unwrap()
         .0;
-    println!("split_ix: {}", split_ix);
     let input_ids_lines = trimmed_input[..split_ix].to_vec();
-    println!("split lines: {}", input_ids_lines.len());
-    let input_ids = input_ids_lines
+    let mut input_ids = input_ids_lines
         .iter()
         .map(|line| parse_fresh_id_set_line(line))
-        .collect::<HashSet<(usize, usize)>>();
-    println!("input ids: {}", input_ids.len());
+        .collect::<Vec<(usize, usize)>>();
+    input_ids.sort();
 
     let input_requests = trimmed_input[split_ix + 1..]
         .iter()
@@ -53,7 +54,7 @@ fn parse_input(input: &str) -> (HashSet<(usize, usize)>, HashSet<usize>) {
 }
 
 fn requested_fresh_ids_count_naive(
-    fresh_ids: &HashSet<(usize, usize)>,
+    fresh_ids: &[(usize, usize)],
     requested_ids: &HashSet<usize>,
 ) -> usize {
     requested_ids
@@ -64,4 +65,36 @@ fn requested_fresh_ids_count_naive(
                 .any(|(lower, upper)| **id >= *lower && **id <= *upper)
         })
         .count()
+}
+
+fn fresh_id_count(sorted_fresh_id_ranges: &[(usize, usize)]) -> u128 {
+    let mut id_count: u128 = 0;
+    let mut previous_range: Option<(usize, usize)> = None;
+
+    for (lower, upper) in sorted_fresh_id_ranges {
+        let current_lower = match previous_range {
+            Some((_, prev_upper)) if *lower <= prev_upper => prev_upper + 1,
+            _ => *lower,
+        };
+
+        if current_lower > *upper {
+            println!(
+                "Skipping range: {}-{}, current_lower={}, state={}",
+                lower, upper, current_lower, id_count
+            );
+            continue;
+        }
+
+        let increment = max((*upper - current_lower) + 1, 0);
+
+        println!(
+            "Processing range: {}-{}, current_lower={}, increment={}, state={}",
+            lower, upper, current_lower, increment, id_count
+        );
+
+        id_count += increment as u128;
+        previous_range = Some((*lower, *upper));
+    }
+
+    id_count
 }
