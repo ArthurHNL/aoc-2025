5d6756d6cc8d79c5029c6c89482e6bc9324c7a3a
Arthur Heidt <arthur@arthurheidt.nl>
2025-12-26 11:02:07 +0100

    day 4

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let input = include_str!("./input_example.txt");
+    let input = include_str!("./input.txt");
     let grid = parse_input(input);
     println!(
         "Total accessible places (naive): {}",
-        total_accessible_places_naive(&grid)
+        accessible_occupied_places(&grid).len()
+    );
+    println!(
+        "Total accessible places (dynamic): {}",
+        removeable_places(&grid)
     );
 }
 
@@ -14,6 +18,22 @@ fn parse_input(input: &str) -> Vec<Vec<bool>> {
         .lines()
         .map(|line| line.trim().chars().map(|c| c == '@').collect())
         .collect()
+}
+
+fn print_grid(grid: &[Vec<bool>]) {
+    for line in grid {
+        for spot in line {
+            print!(
+                "{}",
+                match spot {
+                    true => "@",
+                    false => ".",
+                }
+            )
+        }
+        println!();
+    }
+    println!();
 }
 
 const NEIGHBOUR_OFFSETS: [(isize, isize); 8] = [
@@ -55,15 +75,41 @@ fn get_occupied_neighbours(grid: &[Vec<bool>], y: usize, x: usize) -> usize {
     occupied_neighbours
 }
 
-fn total_accessible_places_naive(grid: &[Vec<bool>]) -> usize {
-    let mut accessible_count = 0;
+fn accessible_occupied_places(grid: &[Vec<bool>]) -> Vec<(usize, usize)> {
+    let mut accessible_places: Vec<(usize, usize)> = Vec::new();
     for (y, line) in grid.iter().enumerate() {
         for (x, spot) in line.iter().enumerate() {
             if *spot && get_occupied_neighbours(grid, y, x) < 4 {
-                accessible_count += 1;
+                accessible_places.push((y, x));
             }
         }
     }
 
-    accessible_count
+    accessible_places
+}
+
+fn prune_grid(grid: &[Vec<bool>], accessible_places: &[(usize, usize)]) -> Vec<Vec<bool>> {
+    let mut new_grid = grid.to_vec();
+    for (y, x) in accessible_places {
+        new_grid[*y][*x] = false;
+    }
+
+    new_grid
+}
+
+fn removable_places_recursive(pruned_grid: &[Vec<bool>], total_removed_count: usize) -> usize {
+    let accessible_places = accessible_occupied_places(pruned_grid);
+    match accessible_places.len() {
+        0 => total_removed_count,
+        _ => {
+            let pruned_grid = prune_grid(pruned_grid, &accessible_places);
+            print_grid(&pruned_grid);
+            removable_places_recursive(&pruned_grid, total_removed_count + accessible_places.len())
+        }
+    }
+}
+
+fn removeable_places(grid: &[Vec<bool>]) -> usize {
+    print_grid(grid);
+    removable_places_recursive(grid, 0)
 }
